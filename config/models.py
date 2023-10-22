dcd00ded37ed720fb0a7052a55706bd5c5f6e19f
kowalski <leejhwalker@gmail.com>
2023-10-22 23:19:50 +0200

    Created Services table added foreign key renamed foreign key twice, migrations 4-6

--- config/models.py
+++ config/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 
 class ServiceCategory(models.Model):
-    code = models.CharField(max_length=10, unique=True)
+    code = models.CharField(max_length=4, unique=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
     image_path = models.CharField(max_length=1000, blank=True, null=True)
     date_created = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = 'service_categories'
+
+class Services(models.Model):
+    code = models.CharField(max_length=4, unique=True)
+    service_category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    total_duration = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_path = models.CharField(max_length=1000, blank=True, null=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'services'
