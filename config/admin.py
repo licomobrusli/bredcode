d223929df49c490896d3bcad2326ecff6477e124
kowalski <leejhwalker@gmail.com>
2023-10-23 15:12:47 +0200

    added admin.py to config app

---
+++ config/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import ServiceCategory, Services
+
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'description', 'image_path', 'date_created']
+    search_fields = ['code', 'name']
+    list_filter = ['date_created']
+
+admin.site.register(ServiceCategory, ServiceCategoryAdmin)
+
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ['code', 'service_category', 'name', 'description', 'total_duration', 'price', 'image_path', 'date_created']
+    search_fields = ['code', 'name']
+    list_filter = ['date_created', 'service_category']
+
+admin.site.register(Services, ServicesAdmin)
